83cbed5e23e721ff7e34297f3a6f7c05a14f2f32
unknown <6692@fje.edu>
2021-12-14 13:15:35 +0100

    prueba

--- processes/login.proc.php
+++ processes/login.proc.php
@@ -3,28 +3,7 @@
 
 include '../services/config.php';
 include '../services/conexion.php';
-/*
-#Hacemos request del usuario y la contraseña que pillamos del login
-$email=$_REQUEST['email'];
-$pass=$_REQUEST['contraseña'];
 
-$email = mysqli_real_escape_string($servidor,$email); //hace que este string no pueda tener carácteres especiales cómo comillas
-#Comprobamos que el usuario y contraseña existan en la tabla ADMIN
-$user = mysqli_query($servidor,"SELECT * FROM tbl_usuarios where email_usuario='$email' and contra_usuario=('{$contraseña}')");
-#Realizamos la query
-#Aqui guardamos el resultado, si es 1 o 0.
-if (mysqli_num_rows($user) == 1) {
-    // coincidencia de credenciales
-    session_start();
-    $_SESSION['email']=$email;
-    header("location: ../view/vista.php");
-} else {
-    // usuario o contraseña erróneos
-    header("location: ../view/login.html");
-}
-
-mysqli_free_result($user);
-*/
 
 //////////////////////////
 
@@ -45,7 +24,7 @@ if (isset($_POST['email']) && isset($_POST['pass'])) {
             //header("location:../view/vista.php?email=$email");
             header("location:../view/menu.php?email=$email");
         }else {
-            header("location: ../view/login.html");
+            header("location:../view/login.html");
             //echo '<script language="javascript">alert("Email o contraseña incorrectos");</script>';
             
         }
@@ -53,5 +32,5 @@ if (isset($_POST['email']) && isset($_POST['pass'])) {
         echo $e->getMessage();
     }
 }else{
-    header("location: ../view/login.html");
+    header("location:../view/login.html");
 }
