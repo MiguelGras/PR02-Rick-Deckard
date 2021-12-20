3ff66fa34fb0af94816b69c4f9150a9063c06d62
Miquel <6692.joan23@fje.edu>
2021-12-20 16:27:18 +0100

    cambio de titulo en archivos e icono, cambios finales

--- processes/admin/formulario.modificar.php
+++ processes/admin/formulario.modificar.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-<title>Modificar mesa</title>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../css/vista.css">
+    <link rel="shortcut icon" type="image/png" href="../../img/icono.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../../js/modalbox.js"></script>
     <script src="../../js/login.js"></script>
-    <title>Vista Proyecto RICK-DECKARD21</title>
+    <title>Modificar usuario</title>
 </head>
 
 <body>
--- view/camarero/historial.php
+++ view/camarero/historial.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../css/vista.css">
+    <link rel="shortcut icon" type="image/png" href="../../img/icono.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../../js/modalbox.js"></script>
     <script src="../../js/vista.js"></script>
-    <title>Vista Proyecto RICK-DECKARD21</title>
+    <title>Historial</title>
 </head>
 <body>
 <div class='paddingtop'>
